bffdb8f17f4b25e0088528e3a66d03e87f6c273c
Bill <wang860218bill@gmail.com>
2024-09-08 00:14:46 +0800

    補充註解

--- TaskSchedulerTemplate/Controllers/HomeController.cs
+++ TaskSchedulerTemplate/Controllers/HomeController.cs
@@ -17,36 +17,45 @@ namespace TaskSchedulerTemplate.Controllers
             _register = register;
         }
 
+        //登入畫面
         public IActionResult Index()
         {
             return View();
         }
 
+        //註冊畫面
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
+        //註冊功能
         [HttpPost]
         public IActionResult Register(RegisterViewModel model)
         {
+            //如果資料不符合規則
             if (!ModelState.IsValid)
             {
+                //回到註冊畫面並顯示錯誤訊息
                 return View("Register", model);
             }
 
+            //檢查帳號是否重複
             bool HasAc = _register.AcHasBeenUsed(model.Member_Account_);
 
             if (HasAc)
             {
+                //帳號重複，回到註冊畫面並顯示錯誤訊息
                 return View("Register", model);
             }
             else
             {
+                //進行註冊
                 _register.RegisterAccount(model);
             }
 
+            //回到登入畫面
             return Redirect("Index");
         }
 
